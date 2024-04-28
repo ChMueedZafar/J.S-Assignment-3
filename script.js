639a976135b2b26e8e452ce1d6d0a748493078cf
ChMueedZafar <chmueedzafar@gmail.com>
2024-04-28 16:49:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Question no 1
+let str = "AbdulMueed"
+function findlenght(){
+    return str.length
+}
+console.log(str.length);
+// Question no 2
+let str1 = "chMueed"
+let str2 = "Zafar"
+const newstr = str1.concat(str2)
+console.log(newstr);
+// Question no 3
+let string = "e"
+if (string){
+    console.log("string is empty" );
+}
+else{
+    console.log("string is not empty")
+}
+// Question no 4
+let strg= "mueed"
+function (){
+    return countchar
+}
+console.log(vowalwords.strg);
